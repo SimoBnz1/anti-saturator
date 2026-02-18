4ecba9b6bb90d7b63becab4352a7b9ee1015adca
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-02-18 11:37:35 +0000

    teminer la page de Quiz

---
+++ src/modules/quiz.js
@@ -0,0 +1,20 @@
+
+let question1 = document.getElementById('question1');
+let question2 =document.getElementById('question2');
+let question3 = document.getElementById('question3');
+let result = document.getElementById('result');
+
+function addQuize(){
+    let scor = Number(question1.value) + Number(question2.value) + Number(question3.value);
+    let average = scor / 3;
+    result.textContent = 'Résultat: ' + average.toFixed(1);
+}
+question1.addEventListener('input' , addQuize);
+question2.addEventListener('input' , addQuize);
+question3.addEventListener('input' , addQuize);
+
+addQuize();
+
+
+
+
