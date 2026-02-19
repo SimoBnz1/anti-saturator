160a05c78f7bffea73513d3e3288513a53db6a68
mohamed_bnz <simobnz81@gmail.com>
2026-02-19 09:46:28 +0000

    ajouter la fonction qui calculer le score d'une tache

--- src/modules/sorter.js
+++ src/modules/sorter.js
@@ -4,3 +4,10 @@ let tasks=[
   { id: "2", title: "Task B", importance: 5, urgence: 1, effort: 1, skipped: false },
 ]
 
+
+let energy=5;
+
+function calculateScore(task, energy) {
+  const adjustedEffort = task.effort * (10 - energy) / 10;
+  return task.importance + task.urgence - adjustedEffort;
+}
